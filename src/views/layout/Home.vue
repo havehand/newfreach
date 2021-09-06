e65312d11d284fa13cd7569471f9d1d34c409bcd
jiluqi <1210900889@qq.com>
2021-09-06 19:46:14 +0800

    当前是整体的布局

---
+++ src/views/layout/Home.vue
@@ -0,0 +1,121 @@
+<template>
+  <div class="home">
+    <el-container>
+
+      <el-aside width="200px">
+        <div class="menu-list">
+          <el-menu
+              default-active="2"
+              class="el-menu-vertical-demo"
+              @open="handleOpen"
+              @close="handleClose"
+              background-color="#545c64"
+              text-color="#fff"
+              active-text-color="#ffd04b">
+            <el-submenu index="1">
+              <template slot="title">
+                <i class="el-icon-location"></i>
+                <span>导航一</span>
+              </template>
+              <el-menu-item-group>
+                <template slot="title">分组一</template>
+                <el-menu-item index="1-1">选项1</el-menu-item>
+                <el-menu-item index="1-2">选项2</el-menu-item>
+              </el-menu-item-group>
+              <el-menu-item-group title="分组2">
+                <el-menu-item index="1-3">选项3</el-menu-item>
+              </el-menu-item-group>
+              <el-submenu index="1-4">
+                <template slot="title">选项4</template>
+                <el-menu-item index="1-4-1">选项1</el-menu-item>
+              </el-submenu>
+            </el-submenu>
+            <el-menu-item index="2">
+              <i class="el-icon-menu"></i>
+              <span slot="title">导航二</span>
+            </el-menu-item>
+            <el-menu-item index="3" disabled>
+              <i class="el-icon-document"></i>
+              <span slot="title">导航三</span>
+            </el-menu-item>
+            <el-menu-item index="4">
+              <i class="el-icon-setting"></i>
+              <span slot="title">导航四</span>
+            </el-menu-item>
+          </el-menu>
+        </div>
+      </el-aside>
+
+      <el-container class="right">
+
+        <el-header>
+          <div class="breadcrumb">
+            <el-breadcrumb separator="/">
+              <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+              <el-breadcrumb-item><a href="/">活动管理</a></el-breadcrumb-item>
+              <el-breadcrumb-item>活动列表</el-breadcrumb-item>
+              <el-breadcrumb-item>活动详情</el-breadcrumb-item>
+            </el-breadcrumb>
+            <div class="user-info">
+              <el-dropdown>
+                <span class="el-dropdown-link">
+                  欢迎xxx<i class="el-icon-arrow-down el-icon--right"></i>
+                </span>
+                <el-dropdown-menu slot="dropdown">
+                  <el-dropdown-item>退出</el-dropdown-item>
+                </el-dropdown-menu>
+              </el-dropdown>
+            </div>
+          </div>
+        </el-header>
+
+        <el-main>
+
+        </el-main>
+      </el-container>
+
+    </el-container>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "Home",
+  methods: {
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    handleClose(key, keyPath) {
+      console.log(key, keyPath);
+    }
+  }
+}
+</script>
+
+<style scoped lang="less">
+.home{
+  height: 100%;
+  .menu-list{
+    height: 100%;
+    width: 200px;
+    position: fixed;
+    .el-menu-vertical-demo{
+      height: 100%;
+      border: none;
+      transition: all 0.5s;
+    }
+  }
+  .breadcrumb{
+    display: flex;
+    align-items: center;
+    height: 50px;
+    justify-content: space-between;
+    border-bottom: 1px solid #ccc;
+    cursor: pointer;
+  }
+  .user-info{
+    padding-right: 10px;
+  }
+}
+</style>
